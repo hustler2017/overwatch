ba962d559305d94b25decfef98b455e292eefbde
ilya <averin.ilya@inbox.ru>
2018-07-05 02:59:37 +0500

    added fl.ru parser

--- classes/Fl.php
+++ classes/Fl.php
@@ -9,7 +9,20 @@
 class Fl extends Parser
 {
 	public $domain = 'https://www.fl.ru';
-	public $anchor = 'a.b-post__link';
+	public $anchor = 'h2 > a.b-post__link';//'a.b-post__link';
+	public $targets = [
+		'/projects/'
+	];
+
+	public $last_html = '';
+
+
+	public function isForEveryone($string){
+		if(preg_match('/Для всех/', $string)){
+			return true;
+		}
+		return false;
+	}
 
 	public function get($query)
 	{
@@ -21,23 +34,72 @@ class Fl extends Parser
 			)
 		));
 
-		$html = file_get_contents($this->domain.$query['query_string'], false, $context);
+		$html = file_get_contents($this->domain.$query, false, $context);
+		$html = str_replace( array(
+			"<script type=\"text/javascript\">document.write('",
+			"');</script>",
+			"</script>"
+		) , '', $html);
+
+		$this->last_html = $html;
 
 		return $html;
 	}
 
+	public function parseDate($date_string)
+	{
+		$matches = [];
+		if(preg_match('/(\d+) (часов|часа|час) (\d+) (минуту|минут|минуты) назад/', $date_string, $matches)){
+			$hours = intval($matches[1]);
+			$minutes = intval($matches[3]);
+			$offset = (($hours * 60) + $minutes) * 60;
+		} elseif(preg_match('/(\d+) (минуту|минут|минуты) назад/', $date_string, $matches)){
+			$minutes = intval($matches[1]);
+			$offset = $minutes * 60;
+		} elseif(preg_match('/Только что/', $date_string, $matches)){
+			$offset = 0;
+		} else {
+			return false;
+		}
+
+		$published = date("Y-m-d H:i:s", time() - $offset);
+
+		return $published;
+	}
+
+
 	public function parseItem($anchor)
 	{
-		$href = $this->domain.pq($anchor)->attr('href');
-		$id = pq($anchor)->attr('id');
-		$id = (int)substr($id,-7,7);
-		$title = $anchor->nodeValue;
+
+		$link = pq($anchor);
+		$href = $link->attr('href');
+		$title = $link->html();
+
+		$container = $link->parent()->parent();
+		if($container->length){
+			$to_parser = $container->get(0)->textContent;
+		} else {
+			return false;
+		}
+
+		// только общедоступные задания
+		if(false === $this->isForEveryone($to_parser))
+			return false;
+
+		$published = $this->parseDate($to_parser);
+		if($published === false)
+		{
+			return false;
+		}
+
+
 
 		return [
-			'id' => $id,
+			'domain' => 'fl',
+			'url' => $href,
 			'title' => $title,
-			'href' => $href,
-			'time' => '',
+			'published' => $published,
+			'founded' => date("Y-m-d H:i:s", time() ),
 			'description' => ''
 		];
 	}
